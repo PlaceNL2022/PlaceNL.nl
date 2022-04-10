a7f7e01247c9b8aa7d05f4ed9096184e8c4733ad
Noah van der Aa <ndvdaa@gmail.com>
2022-04-10 11:30:31 +0200

    explicitly specify icon height

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -3,7 +3,7 @@ import { Link, NavLink } from 'react-router-dom';
 export default () => <div className='bg-gray-800 p-2 sm:flex gap-2 w-full'>
     <div className='flex-auto'>
         <Link to='/' className='hover:bg-gray-700 p-2 flex w-max'>
-            <img src='/logo512.png' width={28} alt='' />&nbsp;Place NL
+            <img src='/logo512.png' width={28} height={28} alt='' />&nbsp;Place NL
         </Link>
     </div>
     <div className='flex-auto flex justify-end gap-2'>
